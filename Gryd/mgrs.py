2edab005731423db21171bb770d840cfeb0a217c
Moustikitos <moustikitos@gmail.com>
2021-01-01 21:17:50 +0100

    flake8 compliancy

--- Gryd/mgrs.py
+++ Gryd/mgrs.py
@@ -1,61 +1,79 @@
 # -*- encoding:utf-8 -*-
 # Military Grid Reference System
-"""Copyright© 2015-2016, THOORENS Bruno
-All rights reserved"""
 
 from . import Grid, Geodesic, ctypes, utm, geoid
 from math import radians
 
-MD = geoid.MD # meridian distance
+MD = geoid.MD  # meridian distance
 MD.argtypes = [ctypes.c_double, ctypes.c_double, ctypes.c_double]
 MD.restype = ctypes.c_double
 
 ENGINE_F = utm.forward
 ENGINE_I = utm.inverse
 
+
 def forward(crs, lla):
-	grid = ENGINE_F(crs, lla)
+    grid = ENGINE_F(crs, lla)
 
-	col = int(grid.easting//100000.0)
-	row = int(grid.northing//100000.0)
-	grid.easting -= (col*100000.0)
-	grid.northing -= (row*100000.0)
+    col = int(grid.easting // 100000.0)
+    row = int(grid.northing // 100000.0)
+    grid.easting -= (col * 100000.0)
+    grid.northing -= (row * 100000.0)
 
-	E_band = E_letter[int(grid.area[:-1])%3]
-	N_band = (N_shifted_letter if crs.datum.ellipsoid.epsg in [7004, 7006, 7008, 7012] else N_letter)[int(grid.area[:-1])%2]
+    E_band = E_letter[int(grid.area[:-1]) % 3]
+    N_band = (
+        N_shifted_letter
+        if crs.datum.ellipsoid.epsg in [7004, 7006, 7008, 7012]
+        else N_letter
+    )[int(grid.area[:-1]) % 2]
 
-	grid.area = "%s %s" % (grid.area, E_band[col-1] + N_band[row%len(N_band)])
-	return grid
+    grid.area = "%s %s" % (
+        grid.area, E_band[col-1] + N_band[row % len(N_band)]
+    )
+    return grid
+
 
 def inverse(crs, grid):
-	fuseau, area = grid.area.split()
-	fuseau, zone = int(fuseau[:-1]), fuseau[-1]
+    fuseau, area = grid.area.split()
+    fuseau, zone = int(fuseau[:-1]), fuseau[-1]
 
-	ellipsoid = crs.datum.ellipsoid
-	k0 = crs.k0
+    ellipsoid = crs.datum.ellipsoid
+    k0 = crs.k0
 
-	col = E_letter[fuseau%3].index(area[0])+1
-	row = (N_shifted_letter if ellipsoid.epsg in [7004, 7006, 7008, 7012] else N_letter)[fuseau%2].index(area[-1])
+    col = E_letter[fuseau % 3].index(area[0])+1
+    row = (
+        N_shifted_letter
+        if ellipsoid.epsg in [7004, 7006, 7008, 7012]
+        else N_letter
+    )[fuseau % 2].index(area[-1])
 
-	northing = k0 * MD(ellipsoid.a, ellipsoid.e, radians(float(UTM_letter[zone])))
-	grid.easting += col * 100000.
-	grid.northing += ((northing//2000000)*20 + row) * 100000.
+    northing = k0 * MD(
+        ellipsoid.a, ellipsoid.e, radians(float(UTM_letter[zone]))
+    )
+    grid.easting += col * 100000.
+    grid.northing += ((northing // 2000000) * 20 + row) * 100000.
 
-	grid.area = "%s%s" % (fuseau, zone)
-	return ENGINE_I(crs, grid)
+    grid.area = "%s%s" % (fuseau, zone)
+    return ENGINE_I(crs, grid)
 
- 
+
 E_letter = {
-	# key = UTMZONENUMBER%3
-	1.: ["A", "B", "C", "D", "E", "F", "G", "H"],
-	2.: ["J", "K", "L", "M", "N", "P", "Q", "R"],
-	0.: ["S", "T", "U", "V", "W", "X", "Y", "Z"]
+    # key = UTMZONENUMBER % 3
+    1.: ["A", "B", "C", "D", "E", "F", "G", "H"],
+    2.: ["J", "K", "L", "M", "N", "P", "Q", "R"],
+    0.: ["S", "T", "U", "V", "W", "X", "Y", "Z"]
 }
 
 N_letter = {
-	# key = UTMZONENUMBER%2
-	1.: ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V"],
-	0.: ["F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "A", "B", "C", "D", "E"]
+    # key = UTMZONENUMBER%2
+    1.: [
+        "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P",
+        "Q", "R", "S", "T", "U", "V"
+    ],
+    0.: [
+        "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U",
+        "V", "A", "B", "C", "D", "E"
+    ]
 }
 
 # for specific ellipsoid :
@@ -64,12 +82,19 @@ N_letter = {
 # Clarke 1866
 # Clarke 1880
 N_shifted_letter = {
-	# key = UTMZONENUMBER%2
-	1.: ["L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "A", "B", "C", "D", "E", "F", "G", "H", "J", "K"],
-	0.: ["R", "S", "T", "U", "V", "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q"]
+    # key = UTMZONENUMBER % 2
+    1.: [
+        "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "A", "B", "C", "D",
+        "E", "F", "G", "H", "J", "K"
+    ],
+    0.: [
+        "R", "S", "T", "U", "V", "A", "B", "C", "D", "E", "F", "G", "H", "J",
+        "K", "L", "M", "N", "P", "Q"
+    ]
 }
 
 UTM_letter = {
-	"X": 72, "W": 64,  "V": 56,  "U": 48,  "T": 40,  "S": 32,  "R": 24,  "Q": 16,  "P": 8,   "N": 0,
-	"M": -8, "L": -16, "K": -24, "J": -32, "H": -40, "G": -48, "F": -56, "E": -64, "D": -72, "C": -80
+    "X": 72, "W": 64, "V": 56, "U": 48, "T": 40, "S": 32, "R": 24, "Q": 16,
+    "P": 8, "N": 0, "M": -8, "L": -16, "K": -24, "J": -32, "H": -40, "G": -48,
+    "F": -56, "E": -64, "D": -72, "C": -80
 }
--- Gryd/utm.py
+++ Gryd/utm.py
@@ -1,7 +1,5 @@
 # -*- encoding:utf-8 -*-
 # Universal Tranverse Mercator
-"""Copyright© 2015-2016, THOORENS Bruno
-All rights reserved"""
 
 from . import *
 from math import pi, radians, degrees
@@ -9,96 +7,111 @@ from math import pi, radians, degrees
 ENGINE_F = tmerc_forward
 ENGINE_I = tmerc_inverse
 
+
 def forward(crs, lla):
-	ZoneNumber = _UTMZoneNumber((lla.longitude+pi) - int((lla.longitude+pi)/(2*pi))*2*pi - pi, lla.latitude)
+    ZoneNumber = _UTMZoneNumber(
+        (lla.longitude + pi) - int(
+            (lla.longitude + pi) / (2 * pi)
+        ) * 2 * pi - pi,
+        lla.latitude
+    )
 
-	if crs.datum.ellipsoid.a == 0: crs.datum = "WGS 84"
-	crs.lambda0 = radians((ZoneNumber-1)*6-180+3)
-	crs.phi0 = 0.
-	crs.y0 = 10000000.0 if lla.latitude < 0 else 0.
-	crs.x0 = 500000.0
-	crs.k0 = 0.9996
+    if crs.datum.ellipsoid.a == 0:
+        crs.datum = "WGS 84"
 
-	xya = ENGINE_F(crs, lla)
-	return Grid(northing=xya.y, easting=xya.x, altitude=lla.altitude, area="%d%c" % (ZoneNumber, _UTMLetterDesignator(lla.latitude)))
+    crs.lambda0 = radians((ZoneNumber-1)*6-180+3)
+    crs.phi0 = 0.
+    crs.y0 = 10000000.0 if lla.latitude < 0 else 0.
+    crs.x0 = 500000.0
+    crs.k0 = 0.9996
+
+    xya = ENGINE_F(crs, lla)
+    return Grid(
+        northing=xya.y, easting=xya.x, altitude=lla.altitude,
+        area="%d%c" % (ZoneNumber, _UTMLetterDesignator(lla.latitude))
+    )
+
 
 def inverse(crs, grid):
-	if crs.datum.ellipsoid.a == 0: crs.datum = "WGS 84"
-	crs.lambda0 = radians((int(grid.area[:-1])-1)*6-180+3)
-	crs.phi0 = 0.
-	crs.y0 = 10000000.0 if grid.area[-1] < 'N' else 0.
-	crs.x0 = 500000.0
-	crs.k0 = 0.9996
+    if crs.datum.ellipsoid.a == 0: crs.datum = "WGS 84"
+    crs.lambda0 = radians((int(grid.area[:-1]) - 1) * 6 - 180 + 3)
+    crs.phi0 = 0.
+    crs.y0 = 10000000.0 if grid.area[-1] < 'N' else 0.
+    crs.x0 = 500000.0
+    crs.k0 = 0.9996
 
-	return ENGINE_I(crs, Geographic(x=grid.easting, y=grid.northing, altitude=grid.altitude))
+    return ENGINE_I(
+        crs,
+        Geographic(x=grid.easting, y=grid.northing, altitude=grid.altitude)
+    )
+
 
 def _UTMZoneNumber(lambd_, phi):
-	lambd_, phi = degrees(lambd_), degrees(phi)
-	# if phi >= 56.0 and phi < 64.0 and lambd_ >= 3.0 and lambd_ < 12.0:
-	if 64.0 > phi >= 56.0 and 12.0 > lambd_ >= 3.0:
-		return 32
-	# Special zones for Svalbard
-	# if phi >= 72.0 and phi < 84.0:
-	if 72.0 <= phi < 84.0:
-		if   0.0 <= lambd_ < 9.0:  return 31
-		elif        lambd_ < 21.0: return 33
-		elif        lambd_ < 33.0: return 35
-		elif        lambd_ < 42.0: return 37
+    lambd_, phi = degrees(lambd_), degrees(phi)
+    # if phi >= 56.0 and phi < 64.0 and lambd_ >= 3.0 and lambd_ < 12.0:
+    if 64.0 > phi >= 56.0 and 12.0 > lambd_ >= 3.0:
+        return 32
+    # Special zones for Svalbard
+    # if phi >= 72.0 and phi < 84.0:
+    if 72.0 <= phi < 84.0:
+        if   0.0 <= lambd_ < 9.0:
+            return 31
+        elif lambd_ < 21.0:
+            return 33
+        elif lambd_ < 33.0:
+            return 35
+        elif lambd_ < 42.0:
+            return 37
+    return int((lambd_ + 180) / 6) + 1
 
-		# if   lambd_ >= 0.0  and lambd_ < 9.0:  return 31
-		# elif lambd_ >= 9.0  and lambd_ < 21.0: return 33
-		# elif lambd_ >= 21.0 and lambd_ < 33.0: return 35
-		# elif lambd_ >= 33.0 and lambd_ < 42.0: return 37
-	return int((lambd_+180)/6)+1
 
 def _UTMLetterDesignator(lat):
-	"""
-	This routine determines the correct UTM letter designator for the given latitude
-	returns 'Z' if latitude is outside the UTM limits of 84N to 80S
-	"""
-	lat = degrees(lat)
-	if   lat >= 84:  return 'Z'
-	elif lat >= 72:  return 'X' # 84  >= lat >= 72:  return 'X'
-	elif lat >= 64:  return 'W' # 72  >  lat >= 64:  return 'W'
-	elif lat >= 56:  return 'V' # 64  >  lat >= 56:  return 'V'
-	elif lat >= 48:  return 'U' # 56  >  lat >= 48:  return 'U'
-	elif lat >= 40:  return 'T' # 48  >  lat >= 40:  return 'T'
-	elif lat >= 32:  return 'S' # 40  >  lat >= 32:  return 'S'
-	elif lat >= 24:  return 'R' # 32  >  lat >= 24:  return 'R'
-	elif lat >= 16:  return 'Q' # 24  >  lat >= 16:  return 'Q'
-	elif lat >= 8:   return 'P' # 16  >  lat >= 8:   return 'P'
-	elif lat >= 0:   return 'N' # 8   >  lat >= 0:   return 'N'
-	elif lat >= -8:  return 'M' # 0   >  lat >= -8:  return 'M'
-	elif lat >= -16: return 'L' # -8  >  lat >= -16: return 'L'
-	elif lat >= -24: return 'K' # -16 >  lat >= -24: return 'K'
-	elif lat >= -32: return 'J' # -24 >  lat >= -32: return 'J'
-	elif lat >= -40: return 'H' # -32 >  lat >= -40: return 'H'
-	elif lat >= -48: return 'G' # -40 >  lat >= -48: return 'G'
-	elif lat >= -56: return 'F' # -48 >  lat >= -56: return 'F'
-	elif lat >= -64: return 'E' # -56 >  lat >= -64: return 'E'
-	elif lat >= -72: return 'D' # -64 >  lat >= -72: return 'D'
-	elif lat >= -80: return 'C' # -72 >  lat >= -80: return 'C'
-	else:            return 'Z' # if the Latitude is outside the UTM limits
-
-	# if   84  >= lat >= 72:  return 'X'
-	# elif 72  >  lat >= 64:  return 'W'
-	# elif 64  >  lat >= 56:  return 'V'
-	# elif 56  >  lat >= 48:  return 'U'
-	# elif 48  >  lat >= 40:  return 'T'
-	# elif 40  >  lat >= 32:  return 'S'
-	# elif 32  >  lat >= 24:  return 'R'
-	# elif 24  >  lat >= 16:  return 'Q'
-	# elif 16  >  lat >= 8:   return 'P'
-	# elif 8   >  lat >= 0:   return 'N'
-	# elif 0   >  lat >= -8:  return 'M'
-	# elif -8  >  lat >= -16: return 'L'
-	# elif -16 >  lat >= -24: return 'K'
-	# elif -24 >  lat >= -32: return 'J'
-	# elif -32 >  lat >= -40: return 'H'
-	# elif -40 >  lat >= -48: return 'G'
-	# elif -48 >  lat >= -56: return 'F'
-	# elif -56 >  lat >= -64: return 'E'
-	# elif -64 >  lat >= -72: return 'D'
-	# elif -72 >  lat >= -80: return 'C'
-	# else:                   return 'Z' # if the Latitude is outside the UTM limits
-
+    """
+    This routine determines the correct UTM letter designator for the given
+    latitude returns 'Z' if latitude is outside the UTM limits of 84N to 80S
+    """
+    lat = degrees(lat)
+    if lat >= 84:
+        return 'Z'
+    elif lat >= 72:
+        return 'X'
+    elif lat >= 64:
+        return 'W'
+    elif lat >= 56:
+        return 'V'
+    elif lat >= 48:
+        return 'U'
+    elif lat >= 40:
+        return 'T'
+    elif lat >= 32:
+        return 'S'
+    elif lat >= 24:
+        return 'R'
+    elif lat >= 16:
+        return 'Q'
+    elif lat >= 8:
+        return 'P'
+    elif lat >= 0:
+        return 'N'
+    elif lat >= -8:
+        return 'M'
+    elif lat >= -16:
+        return 'L'
+    elif lat >= -24:
+        return 'K'
+    elif lat >= -32:
+        return 'J'
+    elif lat >= -40:
+        return 'H'
+    elif lat >= -48:
+        return 'G'
+    elif lat >= -56:
+        return 'F'
+    elif lat >= -64:
+        return 'E'
+    elif lat >= -72:
+        return 'D'
+    elif lat >= -80:
+        return 'C'
+    else:
+        return 'Z'  # if the Latitude is outside the UTM limits
