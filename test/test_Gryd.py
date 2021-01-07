65a88df8859f356e5453856f63f0ed8fe5f372c7
Moustikitos <moustikitos@gmail.com>
2021-01-07 21:05:34 +0100

    precision evaluation changed

--- test/test_Gryd.py
+++ test/test_Gryd.py
@@ -12,14 +12,14 @@ class Test(unittest.TestCase):
     def test_dms(self):
         for i in range(100):
             value = random.random() * 360.
-            # precision +/- 0.5 mm
+            # precision +/- 0.31 mm on WGS 84 equator
             self.assertAlmostEqual(value, float(Gryd.dms(value)), places=10)
             self.assertAlmostEqual(-value, float(Gryd.dms(-value)), places=10)
 
     def test_dmm(self):
         for i in range(100):
             value = random.random() * 360.
-            # precision +/- 0.5 mm
+            # precision +/- 0.31 mm on WGS 84 equator
             self.assertAlmostEqual(value, float(Gryd.dmm(value)), places=10)
             self.assertAlmostEqual(-value, float(Gryd.dmm(-value)), places=10)
 
@@ -31,7 +31,7 @@ class Test(unittest.TestCase):
         vdest = wgs84.destination(
             london, math.degrees(vdist.final_bearing) + 180, vdist.distance
         )
-        # precision +/- 3mm
+        # precision +/- 3.1 cm on WGS 84 equator
         self.assertAlmostEqual(vdest.longitude, dublin.longitude, places=8)
         self.assertAlmostEqual(vdest.latitude, dublin.latitude, places=8)
 
@@ -41,7 +41,7 @@ class Test(unittest.TestCase):
         dublin = Gryd.Geodesic(-6.259437, 53.350765, 0.)
         dubl_2 = wgs84.lla(wgs84.xyz(dublin))
         dubl_3 = airy.transform(wgs84, wgs84.transform(airy, dublin))
-        # precision +/- 3mm
+        # precision +/- 3.1 cm on WGS 84 equator
         self.assertAlmostEqual(dublin.longitude, dubl_2.longitude, places=8)
         self.assertAlmostEqual(dublin.latitude, dubl_2.latitude, places=8)
         self.assertAlmostEqual(dublin.longitude, dubl_3.longitude, places=8)
